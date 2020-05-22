0706532cb3e31e0f06db32595c5e2453b70f541b
burhanuday <udaipurwalaburhanuddin@gmail.com>
2020-05-22 23:18:34 +0530

    fonts setup

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AppNavigator from "./src/navigation/AppNavigator";
+import { Text, StyleSheet } from "react-native";
+import { useFonts } from "@use-expo/font";
+import { Block } from "./src/components/ui";
+import { AppLoading } from "expo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  const [isLoaded] = useFonts({
+    "SourceSans-ExtraLight": require("./assets/fonts/SourceSansPro-ExtraLight.ttf"),
+    "SourceSans-Light": require("./assets/fonts/SourceSansPro-Light.ttf"),
+    "SourceSans-Regular": require("./assets/fonts/SourceSansPro-Regular.ttf"),
+    "SourceSans-SemiBold": require("./assets/fonts/SourceSansPro-SemiBold.ttf"),
+    "SourceSans-Bold": require("./assets/fonts/SourceSansPro-Bold.ttf"),
+    "SourceSans-Black": require("./assets/fonts/SourceSansPro-Black.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!isLoaded) {
+    return <AppLoading />;
+  } else {
+    const styles = StyleSheet.create({
+      defaultFontFamily: {
+        fontFamily: "SourceSans-Regular",
+      },
+    });
+
+    const oldRender = Text.render;
+    Text.render = function render(...args) {
+      const origin = oldRender.call(this, ...args);
+      return React.cloneElement(origin, {
+        style: [styles.defaultFontFamily, origin.props.style],
+      });
+    };
+    return <AppNavigator />;
+  }
+}
